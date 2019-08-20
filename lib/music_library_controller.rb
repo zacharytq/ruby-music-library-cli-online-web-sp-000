1c72e25a75878420a59abce55ce68b4556fde27c
chic-page-6502 <zacharytq@gmail.com>
2019-08-20 23:18:11 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,7 +9,7 @@ class MusicLibraryController
     puts "Welcome to your music library!"
     holder = nil
     while holder != "exit"
-      puts("Welcome to your music library!")
+      
       puts.("To list all of your songs, enter 'list songs'.")
       puts.("To list all of the artists in your library, enter 'list artists'.")
       puts.("To list all of the genres in your library, enter 'list genres'.")
