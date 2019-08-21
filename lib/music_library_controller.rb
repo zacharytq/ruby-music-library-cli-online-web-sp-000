b20559022b18c074dc2920ac6d1c481742fec4d8
chic-page-6502 <zacharytq@gmail.com>
2019-08-21 00:26:48 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,9 +24,11 @@ class MusicLibraryController
         self.list_songs
       when "list artists"
         self.list_artists
-        self.list_songs_by_artist
       when "list genres"
         self.list_genres
+      when "list artist"
+        self.list_songs_by_artist
+      when "list genre"
         self.list_songs_by_genre
       when "play song"
         self.play_song
