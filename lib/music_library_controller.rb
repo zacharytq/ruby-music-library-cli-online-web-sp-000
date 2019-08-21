20cd665c7e25d60532e9b22c02463c7082f87913
chic-page-6502 <zacharytq@gmail.com>
2019-08-21 00:04:19 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,8 +67,8 @@ class MusicLibraryController
   end
 
   def play_song
-    puts "Which song number would you like to play?"
     self.list_songs
+    puts "Which song number would you like to play?"
     input = gets
 
   end
